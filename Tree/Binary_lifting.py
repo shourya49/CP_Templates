61830a7c608e85cf01ca5c81d989ca2769d16f50
Shourya <shouryainfinix520@gmail.com>
2026-06-05 00:39:29 +0530

    Update Binary_lifting.py

--- Tree/Binary_lifting.py
+++ Tree/Binary_lifting.py
@@ -66,3 +66,31 @@ bl = BinaryLifting(n, adj, 0)
 
 print(bl.kth_parent(4, 1))
 print(bl.kth_parent(4, 2))
+
+LOG = n.bit_length()
+
+up = [[0] * LOG for _ in range(n + 1)]
+
+root = 1
+
+stack = [root]
+vis = [False] * (n + 1)
+vis[root] = True
+
+while stack:
+
+    node = stack.pop()
+
+    for nei in adj[node]:
+
+        if vis[nei]:
+            continue
+
+        vis[nei] = True
+        up[nei][0] = node
+
+        stack.append(nei)
+
+for j in range(1, LOG):
+    for node in range(1, n + 1):
+        up[node][j] = up[up[node][j - 1]][j - 1]

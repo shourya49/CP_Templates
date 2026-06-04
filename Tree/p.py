b9f65c6fc4ef86ed8e8cabda7393bbfa5b080167
Shourya <shouryainfinix520@gmail.com>
2026-06-05 00:36:09 +0530

    Add files via upload

---
+++ Tree/p.py
@@ -0,0 +1,68 @@
+class BinaryLifting:
+
+    def __init__(self, n, adj, root=0):
+
+        self.n = n
+        self.LOG = n.bit_length()
+
+        self.up = [[-1] * self.LOG for _ in range(n)]
+
+        self.build(adj, root)
+
+    def build(self, adj, root):
+
+        stack = [root]
+        visited = [False] * self.n
+        visited[root] = True
+
+        while stack:
+
+            node = stack.pop()
+
+            for nei in adj[node]:
+
+                if visited[nei]:
+                    continue
+
+                visited[nei] = True
+
+                self.up[nei][0] = node
+
+                for j in range(1, self.LOG):
+
+                    parent = self.up[nei][j - 1]
+
+                    if parent != -1:
+                        self.up[nei][j] = self.up[parent][j - 1]
+
+                stack.append(nei)
+
+    def kth_parent(self, node, k):
+
+        for j in range(self.LOG):
+
+            if (k >> j) & 1:
+
+                if node == -1:
+                    return -1
+
+                node = self.up[node][j]
+
+        return node
+    
+n = 7
+
+adj = [
+    [1,2],
+    [0,3,4],
+    [0,5,6],
+    [1],
+    [1],
+    [2],
+    [2]
+]
+
+bl = BinaryLifting(n, adj, 0)
+
+print(bl.kth_parent(4, 1))
+print(bl.kth_parent(4, 2))

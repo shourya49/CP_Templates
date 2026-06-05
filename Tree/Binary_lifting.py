fda9285a0574076ae184d74c8a9574ac391be3a1
Shourya <shouryainfinix520@gmail.com>
2026-06-05 17:02:50 +0530

    Update Binary_lifting.py

--- Tree/Binary_lifting.py
+++ Tree/Binary_lifting.py
@@ -1,71 +1,3 @@
-class BinaryLifting:
-
-    def __init__(self, n, adj, root=0):
-
-        self.n = n
-        self.LOG = n.bit_length()
-
-        self.up = [[-1] * self.LOG for _ in range(n)]
-
-        self.build(adj, root)
-
-    def build(self, adj, root):
-
-        stack = [root]
-        visited = [False] * self.n
-        visited[root] = True
-
-        while stack:
-
-            node = stack.pop()
-
-            for nei in adj[node]:
-
-                if visited[nei]:
-                    continue
-
-                visited[nei] = True
-
-                self.up[nei][0] = node
-
-                for j in range(1, self.LOG):
-
-                    parent = self.up[nei][j - 1]
-
-                    if parent != -1:
-                        self.up[nei][j] = self.up[parent][j - 1]
-
-                stack.append(nei)
-
-    def kth_parent(self, node, k):
-
-        for j in range(self.LOG):
-
-            if (k >> j) & 1:
-
-                if node == -1:
-                    return -1
-
-                node = self.up[node][j]
-
-        return node
-    
-n = 7
-
-adj = [
-    [1,2],
-    [0,3,4],
-    [0,5,6],
-    [1],
-    [1],
-    [2],
-    [2]
-]
-
-bl = BinaryLifting(n, adj, 0)
-
-print(bl.kth_parent(4, 1))
-print(bl.kth_parent(4, 2))
 
 LOG = n.bit_length()
 
@@ -94,3 +26,15 @@ while stack:
 for j in range(1, LOG):
     for node in range(1, n + 1):
         up[node][j] = up[up[node][j - 1]][j - 1]
+
+def kth_ancestor(node, k):
+
+    for j in range(LOG):
+
+        if k & (1 << j):
+            node = up[node][j]
+
+            if node == 0:
+                break
+
+    return node

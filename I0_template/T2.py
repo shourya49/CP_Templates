a72c485f7a6097ab6c6fafc279867702d3f8dff1
Shourya <shouryainfinix520@gmail.com>
2026-06-07 13:51:34 +0530

    Update T2.py

--- I0_template/T2.py
+++ I0_template/T2.py
@@ -1,1 +1,16 @@
+import sys
+import sys,random,bisect
+from collections import deque,defaultdict
+from heapq import heapify,heappop,heappush
+from itertools import permutations
+from math import gcd,log
 
+sys.setrecursionlimit(300000)
+
+input = sys.stdin.readline
+
+def solve():
+  # code
+
+if __name__ == '__main__':
+  solve()

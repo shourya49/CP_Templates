ece51fec7478ca19d59040260e247973807c0338
Shourya <shouryainfinix520@gmail.com>
2026-06-07 13:50:01 +0530

    Update T1.py

--- I0_template/T1.py
+++ I0_template/T1.py
@@ -1,1 +1,15 @@
+import sys,random,bisect
+from collections import deque,defaultdict
+from heapq import heapify,heappop,heappush
+from itertools import permutations
+from math import gcd,log
 
+input = lambda :sys.stdin.buffer.readline()
+mi = lambda :map(int,input().split())
+li = lambda :list(mi())
+def solve():
+  for _ in range(int(input())):
+      n = int(input())
+      edges = [li() for _ in range(n)]
+if __name__ == '__main__':
+  solve()
